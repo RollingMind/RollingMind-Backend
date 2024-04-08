bfcae2ed7f05c7f1296117bc17baca1e3f38490e
minseo-o <halia0709@gmail.com>
2024-04-08 11:47:01 +0900

    test

---
+++ src/main/java/RollingRolling/RollingMindBackend/RollingMindBackendApplication.java
@@ -0,0 +1,13 @@
+package RollingRolling.RollingMindBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingMindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingMindBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RollingRolling/RollingMindBackend/RollingMindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package RollingRolling.RollingMindBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollingMindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
