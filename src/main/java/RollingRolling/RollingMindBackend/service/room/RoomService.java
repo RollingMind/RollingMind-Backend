3b94e4ea7742ee9c3c97bbebb03dfa57ff4a0ae2
jiyeon11 <leejiyeon415@gmail.com>
2024-04-11 17:47:50 +0900

    ✨ Room Service 추가

---
+++ src/main/java/RollingRolling/RollingMindBackend/service/room/RoomService.java
@@ -0,0 +1,10 @@
+package RollingRolling.RollingMindBackend.service.room;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+}
