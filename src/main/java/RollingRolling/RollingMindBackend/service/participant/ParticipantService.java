a9e09ab66f0d84cbcc52438929e6b8609b78bbf0
jiyeon11 <leejiyeon415@gmail.com>
2024-04-14 00:05:33 +0900

    ✨ Participant Service 추가

---
+++ src/main/java/RollingRolling/RollingMindBackend/service/participant/ParticipantService.java
@@ -0,0 +1,25 @@
+package RollingRolling.RollingMindBackend.service.participant;
+
+import RollingRolling.RollingMindBackend.domain.participant.Participant;
+import RollingRolling.RollingMindBackend.dto.participant.AddParticipantRequest;
+import RollingRolling.RollingMindBackend.repository.participant.ParticipantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ParticipantService {
+    private final ParticipantRepository participantRepository;
+
+    @Transactional
+    public Participant save(AddParticipantRequest request){  //방 참가자 테이블에 저장
+        Participant participant = Participant.builder()
+                .room_id(request.getRoom_id())
+                .user_id(request.getUser_id())
+                .status(request.getStatus())
+                .build();
+
+        return participantRepository.save(participant);
+    }
+}
