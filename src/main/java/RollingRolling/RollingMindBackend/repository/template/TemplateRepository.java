bb82350c62adc9d39e5be92e9311b3708dd90c6c
jiyeon11 <leejiyeon415@gmail.com>
2024-05-15 22:54:28 +0900

    ✨ 템플릿 검색 #28

--- src/main/java/RollingRolling/RollingMindBackend/repository/template/TemplateRepository.java
+++ src/main/java/RollingRolling/RollingMindBackend/repository/template/TemplateRepository.java
@@ -1,6 +1,7 @@
 package RollingRolling.RollingMindBackend.repository.template;
 
 import RollingRolling.RollingMindBackend.domain.Template.Template;
+import RollingRolling.RollingMindBackend.domain.room.Room;
 import RollingRolling.RollingMindBackend.dto.Template.TemplateResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,5 @@ import java.util.List;
 public interface TemplateRepository extends JpaRepository<Template, Long> {
     @Query("SELECT t FROM Template t WHERE t.templateId IN :templateIds")
     List<TemplateResponse> findByTemplateIds(List<Long> templateIds);
+    List<Template> findByHashtagContaining(String keyword);
 }
--- src/main/java/RollingRolling/RollingMindBackend/service/template/TemplateService.java
+++ src/main/java/RollingRolling/RollingMindBackend/service/template/TemplateService.java
@@ -14,12 +14,16 @@ import java.util.List;
 public class TemplateService {
     private final TemplateLikesRepository templateLikesRepository;
     private final TemplateRepository templateRepository;
-    public List<TemplateResponse> findTemplatesByMemberNum(int memberNum) {  //ㅈ
+    public List<TemplateResponse> findTemplatesByMemberNum(int memberNum) {
         List<Long> templateIds = templateLikesRepository.findTemplateIdsByMemberNum(memberNum);
         return templateRepository.findByTemplateIds(templateIds);
     }
 
     public List<Template> findAllTemplates(){  //템플릿 전체 목록 조회
         return templateRepository.findAll();
+    }
+
+    public List<Template> searchTemplatesByHashtag(String keyword){
+        return templateRepository.findByHashtagContaining(keyword);
     }
 }
